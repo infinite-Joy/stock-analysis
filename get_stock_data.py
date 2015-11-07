e613d11086ab778992c62cf015f662e09b56cd44
infinite-Joy <joydeepubuntu@gmail.com>
2015-11-07 21:33:06 +0530

    adding get_company_primary_stats refactoring get_company_stock_data

--- get_stock_data.py
+++ get_stock_data.py
@@ -2,29 +2,35 @@ from lxml import html
 import requests
 import time
 
-from try_module import trying as trying
 from company_page import CompanyPage
 
-def company_page_analysis(stock_company):
-    page = requests.get('http://money.rediff.com/%s' % stock_company)
-    time.sleep(2)
-    tree = html.fromstring(page.text)
+def get_company_primary_stats(tree):
     company = CompanyPage(tree)
     pe_ratio = company.get_pe_ratio(tree)
     eps = company.get_eps(tree)
     price_of_stock = company.get_price_of_stock(tree)
     fifty_two_wk_high = company.get_fifty_two_wk_high(tree)
     fifty_two_wk_low = company.get_fifty_two_wk_low(tree)
-    print pe_ratio
-    print eps
-    print price_of_stock
-    print fifty_two_wk_high, fifty_two_wk_low
-    print price_of_stock < ((fifty_two_wk_high - fifty_two_wk_low)/2)
-    
+    return {'pe_ratio': pe_ratio, 'eps': eps, 'price_of_stock': price_of_stock, 'fifty_two_wk_high': fifty_two_wk_high, 'fifty_two_wk_low': fifty_two_wk_low}
+
+def company_page_analysis(stock_company):
+    page = requests.get('http://money.rediff.com/%s' % stock_company)
+    time.sleep(2)
+    tree = html.fromstring(page.text)
+    primary_stats = get_company_primary_stats(tree)
+    print primary_stats
+    if all([primary_stats.get('pe_ratio') < 15, primary_stats.get('eps') > 0, primary_stats.get('price_of_stock') < ((primary_stats.get('fifty_two_wk_high') - primary_stats.get('fifty_two_wk_low'))/2)]):
+        # go to next page
+        return stock_company
+    else:
+        pass
 
 def main():
-    stock_company = "Amtek-Auto-Ltd"
-    company_page_analysis(stock_company)
+    stock_companies = ["Amtek-Auto-Ltd"]
+    companies_to_invest = []
+    for stock_company in stock_companies:
+        companies_to_invest.append(company_page_analysis(stock_company))
+    print companies_to_invest
     
     # balance_sheet = tree.xpath('/html/body/div[4]/div[8]/div[8]/div[2]/div/a[4]/@href')
     # print "".join(balance_sheet)
---
+++ tests/test_get_stock_data.py
@@ -0,0 +1,14 @@
+from lxml import html
+import requests
+import time
+
+from get_stock_data import get_company_primary_stats
+from company_page import CompanyPage
+
+stock_company = "Amtek-Auto-Ltd"
+page = requests.get('http://money.rediff.com/%s' % stock_company)
+tree = html.fromstring(page.text)
+
+def test_get_company_primary_stats():
+    primary_stats = get_company_primary_stats(tree)
+    assert all([primary_stats.get('pe_ratio') > 0, primary_stats.get('eps') > 0, primary_stats.get('price_of_stock') > 0, primary_stats.get('fifty_two_wk_high') > 0, primary_stats.get('fifty_two_wk_low') > 0])
