76fc482cdb5be095d8355f45879ca3c4e595c9b1
joydeep bhattacharjee <joydeep@nineleaps.com>
2017-07-26 22:54:03 +0530

    new xpath style in ratio module

--- ratio_module.py
+++ ratio_module.py
@@ -7,22 +7,22 @@ class Ratio:
         self.tree = tree
 
     def get_present_year_dividend_present_year_dividend_per_share(self, tree):
-        p1 = trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[2]/text()')
+        p1 = trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[2]')
         import pdb
         pdb.set_trace()
         return p1
 
     def get_present_year_dividend_present_year_dividend_per_share_minus1(self, tree):
-        return trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[3]/text()')
+        return trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[3]')
 
     def get_present_year_dividend_present_year_dividend_per_share_minus2(self, tree):
-        return trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[4]/text()')
+        return trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[4]')
 
     def get_present_year_dividend_present_year_dividend_per_share_minus3(self, tree):
-        return trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[5]/text()')
+        return trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[5]')
 
     def get_present_year_dividend_present_year_dividend_per_share_minus4(self, tree):
-        return trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[6]/text()')
+        return trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[6]')
 
     def consistent_dividend_payout(self, tree):
         return all([self.get_present_year_dividend_present_year_dividend_per_share(tree) > 0,
