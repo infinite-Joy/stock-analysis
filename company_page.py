e5d4441e7ff661c65742fc5ff64291e9f19c22cf
joydeep bhattacharjee <joydeep@nineleaps.com>
2017-07-26 19:50:26 +0530

    company page formatting

--- company_page.py
+++ company_page.py
@@ -1,16 +1,15 @@
 from try_module import trying as trying
-from try_module import get_correct_ratio
+
 
 class CompanyPage:
 
     def __init__(self, tree):
         self.tree = tree
 
-
     def _get_correct_ratio(self, tree, x_path_first_part, x_path_sec_part):
         ratio_not_found = True
         i = 8
-        while ratio_not_found == True and i > 0:
+        while ratio_not_found and i > 0:
             x_path = x_path_first_part + str(i) + x_path_sec_part
             ratio = trying(tree, x_path)
             if ratio != 0.00:
@@ -23,34 +22,49 @@ class CompanyPage:
         return pe_ratio
 
     def get_eps(self, tree):
-        eps= trying(tree, '//*[@id="div_rcard_more"]/div[2]/div[2]')
-        # return self._get_correct_ratio(tree, '/html/body/div[4]/div[8]/div[', ']/div[1]/div/div[2]/div[2]/text()')
-        import pdb
-        pdb.set_trace()
+        eps = trying(tree, '//*[@id="div_rcard_more"]/div[2]/div[2]')
         return eps
 
-
     def get_price_of_stock(self, tree):
-        return trying(tree, '//*[@id="ltpid"]/text()')
-
+        pos = trying(tree, '//*[@id="ltpid"]/text()')
+        return pos
 
     def get_fifty_two_wk_high(self, tree):
-        return float(tree.xpath('//*[@id="FiftyTwoHighlow"]/text()')[0].split()[0].replace(",", ""))
-
+        return (
+            float(
+                tree.xpath('//*[@id="FiftyTwoHighlow"]/text()')[0]
+                .split()[0]
+                .replace(",", "")
+            )
+        )
 
     def get_fifty_two_wk_low(self, tree):
-        return float(tree.xpath('//*[@id="FiftyTwoHighlow"]/text()')[0].split()[-1].replace(",", ""))
-
+        return (
+            float(
+                tree.xpath('//*[@id="FiftyTwoHighlow"]/text()')[0]
+                .split()[-1]
+                .replace(",", "")
+            )
+        )
 
     def get_balance_sheet_link(self, tree):
         try:
-            balance_sheet_link1 = tree.xpath('/html/body/div[4]/div[8]/div[8]/div[2]/div/a[4]/@href')[0]
-        except:
+            balance_sheet_link1 = (
+                tree
+                .xpath(
+                    '/html/body/div[4]/div[8]/div[8]/div[2]/div/a[4]/@href'
+                )[0]
+            )
+        except KeyError:
             balance_sheet_link1 = ""
 
         try:
-            balance_sheet_link2 = tree.xpath('/html/body/div[4]/div[8]/div[7]/div[2]/div/a[4]/@href')[0]
-        except:
+            balance_sheet_link2 = (
+                tree.xpath(
+                    '/html/body/div[4]/div[8]/div[7]/div[2]/div/a[4]/@href'
+                )[0]
+            )
+        except KeyError:
             balance_sheet_link2 = ""
 
         if balance_sheet_link1 is not "":
@@ -60,16 +74,17 @@ class CompanyPage:
         else:
             return ""
 
-
     def get_dividend_link(self, tree):
         try:
-            balance_sheet_link1 = tree.xpath('/html/body/div[4]/div[8]/div[7]/div[2]/div/a[7]/@href')[0]
-        except:
+            balance_sheet_link1 = tree.xpath('/html/body/div[4]/div[8]/div[7]'
+                                             '/div[2]/div/a[7]/@href')[0]
+        except KeyError:
             balance_sheet_link1 = ""
 
         try:
-            balance_sheet_link2 = tree.xpath('/html/body/div[4]/div[8]/div[8]/div[2]/div/a[7]/@href')[0]
-        except:
+            balance_sheet_link2 = tree.xpath('/html/body/div[4]/div[8]/div[8]'
+                                             '/div[2]/div/a[7]/@href')[0]
+        except KeyError:
             balance_sheet_link2 = ""
 
         if balance_sheet_link1 is not "":
@@ -81,13 +96,15 @@ class CompanyPage:
 
     def get_ratio_link(self, tree):
         try:
-            balance_sheet_link1 = tree.xpath('/html/body/div[4]/div[8]/div[8]/div[2]/div/a[11]/@href')[0]
-        except:
+            balance_sheet_link1 = tree.xpath('/html/body/div[4]/div[8]/div[8]'
+                                             '/div[2]/div/a[11]/@href')[0]
+        except KeyError:
             balance_sheet_link1 = ""
 
         try:
-            balance_sheet_link2 = tree.xpath('/html/body/div[4]/div[8]/div[7]/div[2]/div/a[11]/@href')[0]
-        except:
+            balance_sheet_link2 = tree.xpath('/html/body/div[4]/div[8]/div[7]'
+                                             '/div[2]/div/a[11]/@href')[0]
+        except KeyError:
             balance_sheet_link2 = ""
 
         if balance_sheet_link1 is not "":
