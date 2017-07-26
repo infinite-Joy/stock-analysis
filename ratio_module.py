017c45f514c1e4f070f3390d11dbfe2f08daab94
joydeep bhattacharjee <joydeep@nineleaps.com>
2017-07-26 23:04:55 +0530

    ratio module refactoring

--- ratio_module.py
+++ ratio_module.py
@@ -1,5 +1,5 @@
 from try_module import trying as trying
-from try_module import get_correct_ratio
+
 
 class Ratio:
 
@@ -8,25 +8,36 @@ class Ratio:
 
     def get_present_year_dividend_present_year_dividend_per_share(self, tree):
         p1 = trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[2]')
-        import pdb
-        pdb.set_trace()
         return p1
 
-    def get_present_year_dividend_present_year_dividend_per_share_minus1(self, tree):
-        return trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[3]')
+    def get_present_year_dividend_present_year_dividend_per_share_minus1(
+        self, tree
+    ):
+        p2 = trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[3]')
+        return p2
 
-    def get_present_year_dividend_present_year_dividend_per_share_minus2(self, tree):
-        return trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[4]')
+    def get_present_year_dividend_present_year_dividend_per_share_minus2(
+        self, tree
+    ):
+        p3 = trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[4]')
+        return p3
 
-    def get_present_year_dividend_present_year_dividend_per_share_minus3(self, tree):
+    def get_present_year_dividend_present_year_dividend_per_share_minus3(
+        self, tree
+    ):
         return trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[5]')
 
-    def get_present_year_dividend_present_year_dividend_per_share_minus4(self, tree):
+    def get_present_year_dividend_present_year_dividend_per_share_minus4(
+        self, tree
+    ):
         return trying(tree, '/html/body/div[2]/div[5]/table/tbody/tr[6]/td[6]')
 
     def consistent_dividend_payout(self, tree):
-        return all([self.get_present_year_dividend_present_year_dividend_per_share(tree) > 0,
-                    self.get_present_year_dividend_present_year_dividend_per_share_minus1(tree) > 0,
-                    self.get_present_year_dividend_present_year_dividend_per_share_minus2(tree) > 0,
-                    self.get_present_year_dividend_present_year_dividend_per_share_minus3(tree) > 0,
-                    self.get_present_year_dividend_present_year_dividend_per_share_minus4(tree) > 0])
+        return all(map(
+            lambda f: f(tree) > 0,
+            [self.get_present_year_dividend_present_year_dividend_per_share,
+             self.get_present_year_dividend_present_year_dividend_per_share_minus1,
+             self.get_present_year_dividend_present_year_dividend_per_share_minus2,
+             self.get_present_year_dividend_present_year_dividend_per_share_minus3,
+             self.get_present_year_dividend_present_year_dividend_per_share_minus4]
+        ))
