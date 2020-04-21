d06a766e8b01968bf5983acfcc000df02fb26afc
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2020-04-21 18:57:26 +0530

    comparison between industry mean and the standard deviation

--- continuous_stock_analysis/stock_trends.py
+++ continuous_stock_analysis/stock_trends.py
@@ -25,7 +25,9 @@ def process_line(line, overall_info={}):
 
 
 all_infos = []
-df = pd.DataFrame(columns=['timestamp', 'tcs', 'infosys', 'hcl', 'tech mahindra', 'oracle finserv', 'mphasis'])
+stocks = ['timestamp', 'tcs', 'infosys', 'hcl', 'tech mahindra', 'oracle finserv', 'mphasis']
+original_cols = [c for c in stocks if c != 'timestamp']
+df = pd.DataFrame(columns=stocks)
 for filename in onlyfiles:
     with open(filename) as f:
         overall_info = {}
@@ -37,14 +39,33 @@ for filename in onlyfiles:
 
 df = df.set_index('timestamp')
 df = df.sort_index()
-print(df)
 
 # now that we have all the df in place we will need to normalise them
 # so that the analysis can be done on an equal level field.
 # we will do percentage change from previous
-for col in df.columns:
+for col in original_cols:
     pc_col = col + '_pc'
     df[pc_col] = df[col].pct_change()
+print('percentage_change calculation done')
 
-print(df)
 
+def is_too_deviated(percentage_change, mean, std):
+    """we will say that the stock is too deviated
+    if the different from the industry mean
+    is more than 2 sigma
+    """
+    diff = percentage_change - mean
+    return diff > 2 * std
+
+# taking the mean of the change in the peer level values.
+pc_cols = [c for c in df.columns if 'pc' in c]
+df['mean_across_peers'] = df[pc_cols].mean(axis=1)
+df['std_dev_across_peers'] = df[pc_cols].std(axis=1)
+print('mean and std dev across peers done')
+df = df.fillna(0.0)
+for percentage_change in pc_cols:
+    is_deviated = percentage_change + '_dev'
+    df[is_deviated] = df.apply(lambda x: is_too_deviated(x[percentage_change], x['mean_across_peers'], x['std_dev_across_peers']), axis=1)
+
+df.to_csv('out.csv')
+print('all done and saved to out.csv')
